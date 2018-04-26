466bd86403196e89b057f3b7670957042aec5463
Sacha vBH <2837880+empirepilot@users.noreply.github.com>
2018-04-26 19:06:04 +0200

    Add files via upload

---
+++ 1/js/main.js
@@ -0,0 +1,30 @@
+let vis = document.createElement("fish")
+document.body.appendChild(vis)
+
+let bubbletje = document.createElement("bubble")
+document.body.appendChild(bubbletje)
+//
+// geef de vis een random positie en een random kleur
+//
+
+let fishes = document.getElementsByTagName("fish")
+let fish = fishes[0]
+fish.style.left = "200px"
+fish.style.top = "100px"
+fish.style.webkitFilter = "hue-rotate(200deg)"
+fish.style.filter = "hue-rotate(200deg)"
+
+//
+// geef de bubble een random positie
+//
+let bubbles = document.getElementsByTagName("bubble")
+let bubble = bubbles[0]
+bubble.style.left = "200px"
+bubble.style.top = "0px"
+
+//
+// roep een functie aan als alles geladen is
+//
+window.addEventListener("load", function () {
+    console.log("start het aquarium")
+})
